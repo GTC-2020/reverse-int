3004d0919d5af7d69b0da37cce14b8f6a4a62853
Turcan Gheorghe <tsurkan.g@gmail.com>
2022-08-03 11:17:56 +0300

    my solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
+  let input = '' + Math.abs(n);
+  let output = "";
+  for(let i = input.length; i > 0; i--){
+    output += input[i - 1];
+  }
+  return Number(output);
   
 }
